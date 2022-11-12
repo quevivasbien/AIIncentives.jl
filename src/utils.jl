36dd98ceb551e8cfc55c322c8a2098cefc0d073b
quevivasbien <jensenm@uchicago.edu>
2022-11-12 14:45:42 -0600

    improved stat functions in utils

--- src/utils.jl
+++ src/utils.jl
@@ -8,22 +8,47 @@ function as_Float64_Array(x::Union{Real, AbstractArray}, n::Int)
     return fill(convert(Float64, x), n)
 end
 
-function mean(x, dim::Integer)
+function broadcast_sum(x, dim::Integer)
     reshape(
-        sum(x, dims = dim) ./ size(x, dim),
-        (s for (i, s) in enumerate(size(x)) if i != dim)...
+        sum(x, dims = dim),
+        (s for (i, s) in enumerate(size(x)) if i!= dim)...
     )
 end
 
-function mean(x, dims::Tuple)
+function broadcast_sum(x, dims::Tuple)
     reshape(
-        sum(x, dims = dims) ./ prod(size(x)[collect(dims)]),
+        sum(x, dims = dims),
         (s for (i, s) in enumerate(size(x)) if i ∉ dims)...
     )
 end
 
+function ax_size(x, dim::Integer)
+    return size(x, dim)
+end
+
+function ax_size(x, dims::Tuple)
+    return prod(size(x)[collect(dims)])
+end
+
+function mean(x, dims)
+    broadcast_sum(x, dims) ./ ax_size(x, dims)
+end
+
 function mean(x)
-    sum(x) ./ length(x)
+    sum(x) / length(x)
+end
+
+function std(x, dims)
+    m = mean(x, dims)
+    # put the dims at the end so we can automatically broadcast with m
+    x_ = PermutedDimsArray(x, (setdiff(1:ndims(x), dims)..., dims...))
+    newdims = dims isa Integer ? ndims(x) : Tuple((ndims(x) - length(dims) + 1):ndims(x))
+    return sqrt.(broadcast_sum((x_ .- m) .^ 2, newdims) ./ (ax_size(x, dims) - 1))
+end
+
+function std(x)
+    m = mean(x)
+    return sqrt(sum((x .- m) .^ 2) / (length(x) - 1))
 end
 
 function to_rowvec(x::Vector)
